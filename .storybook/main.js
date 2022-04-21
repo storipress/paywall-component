6e35796a993e13c09685d74b6257fc3f3194ed49
DanSnow <dododavid006@gmail.com>
2022-04-21 13:16:59 +0800

    chore: set up turbosnap

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,14 @@
-const AutoImport = require('unplugin-auto-import/vite').default
+const turbosnap = require('vite-plugin-turbosnap')
+const AutoImport = require('unplugin-auto-import/vite')
 
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/vue3",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: '@storybook/vue3',
+  core: {
+    builder: '@storybook/builder-vite',
   },
-  viteFianl(config, {configType}) {
+  viteFinal(config, { configType }) {
     config.optimizeDeps =
       configType === 'PRODUCTION'
         ? config.optimizeDeps
@@ -31,8 +27,13 @@ module.exports = {
       AutoImport({
         imports: ['vue', 'vue/macros', 'vue-router', '@vueuse/core', '@vueuse/head'],
         dts: true,
-      }),
+      })
     )
 
-  }
+    if (configType === 'PRODUCTION') {
+      config.plugins.push(turbosnap({ rootDir: config.root }))
+    }
+
+    return config
+  },
 }
