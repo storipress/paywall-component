94a54156a52ce1fb247b5b8d0008e0ccd7088a3e
Harvey <harvey@teaches.cc>
2022-07-18 15:17:27 +0800

    feat: add validators to sign up and sign in

--- src/components/Paywall/Paywall.vue
+++ src/components/Paywall/Paywall.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { ErrorMessage as VeeErrorMessage, Field as VeeField, Form as VeeForm } from 'vee-validate'
+import * as Yup from 'yup'
 import { Button } from '../index'
 import { data } from './data'
 
@@ -17,7 +19,9 @@ const emit = defineEmits<{
   (event: 'clickSignIn'): void
 }>()
 
-const email = ref('')
+const schema = Yup.object().shape({
+  email: Yup.string().email().required(),
+})
 
 const currentData = computed(() => {
   const current = data[props.type]
@@ -31,6 +35,11 @@ const currentData = computed(() => {
 const isUpgrade = computed(() => {
   return props.type === 'upgrade'
 })
+
+const onSubmit = function (formData: any, submitEvent: any) {
+  submitEvent.evt.preventDefault()
+  emit('click', formData.email)
+}
 </script>
 
 <template>
@@ -44,23 +53,24 @@ const isUpgrade = computed(() => {
       </div>
 
       <div class="flex w-full flex-col">
-        <div class="md:flex">
-          <input
-            v-if="!isUpgrade"
-            v-model="email"
-            type="email"
-            placeholder="Email Address"
-            class="text-inputs layer-1 mb-3 h-12 w-full bg-transparent px-4 pt-3.5 pb-[0.8rem] text-zinc-500 md:mb-0 md:mr-[1.125rem] md:max-w-[47.6%]"
-            @keyup.enter="emit('click', email)"
-          />
+        <VeeForm class="md:flex" :validation-schema="schema" @submit="onSubmit">
+          <div v-if="!isUpgrade" class="mb-3 w-full md:mb-0 md:mr-[1.125rem] md:max-w-[47.6%]">
+            <VeeField
+              name="email"
+              type="email"
+              placeholder="Email Address"
+              class="text-inputs layer-1 h-12 w-full bg-transparent px-4 pt-3.5 pb-[0.8rem] text-zinc-500"
+            />
+            <VeeErrorMessage name="email" class="text-caption bottom-[calc(-1.5_*_1em)] text-red-700" />
+          </div>
           <Button
             :text="currentData.button"
             primary
             class="w-full text-lg font-semibold"
             :class="isUpgrade ? 'md:w-[16.125rem]' : 'md:w-[7.5rem]'"
-            @click="emit('click', email)"
+            type="submit"
           />
-        </div>
+        </VeeForm>
         <div v-if="!isUpgrade" class="mt-1.5 text-xs text-zinc-600 md:mt-2.5">
           Already have an account? <button @click="emit('clickSignIn')"><u>Sign in</u></button>
         </div>
