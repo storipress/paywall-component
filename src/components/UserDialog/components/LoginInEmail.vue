999555225ad95ec7a0b0e4e2ea0946e193c651a7
Harvey <harvey@teaches.cc>
2022-07-18 15:17:27 +0800

    feat: add validators to sign up and sign in

--- src/components/UserDialog/components/LoginInEmail.vue
+++ src/components/UserDialog/components/LoginInEmail.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { ErrorMessage as VeeErrorMessage, Field as VeeField, Form as VeeForm } from 'vee-validate'
+import * as Yup from 'yup'
 import { Button } from '../../index'
 import { Email } from '~/components/Icons'
 
@@ -12,28 +14,36 @@ const emit = defineEmits<{
   (event: 'apply', handler: any): void
 }>()
 
-const email = ref('')
+const schema = Yup.object().shape({
+  email: Yup.string().email().required(),
+})
+
+const onSubmit = function (formData: any, submitEvent: any) {
+  submitEvent.evt.preventDefault()
+  emit('apply', formData.email)
+}
 </script>
 
 <template>
-  <div class="flex-auto" v-bind="$attrs">
-    <Button primary rounded color-hex="#ffffff" class="layer-1 mb-6 w-full">
-      <template #buttonText>
-        <Email class="h-6 w-6 text-zinc-700" />
-        <span class="text-button w-full pr-6 text-zinc-700">Log in with Email</span>
-      </template>
-    </Button>
+  <VeeForm :validation-schema="schema" @submit="onSubmit">
+    <div class="relative flex-auto" v-bind="$attrs">
+      <Button primary rounded color-hex="#ffffff" class="layer-1 mb-6 w-full" type="button">
+        <template #buttonText>
+          <Email class="h-6 w-6 text-zinc-700" />
+          <span class="text-button w-full pr-6 text-zinc-700">Log in with Email</span>
+        </template>
+      </Button>
+      <VeeField
+        name="email"
+        type="email"
+        placeholder="Email address"
+        class="text-inputs h-12 w-full border border-zinc-700 bg-transparent px-4 py-3"
+      />
+      <VeeErrorMessage as="div" name="email" class="text-caption absolute bottom-[calc(-1.5_*_1em)] text-red-700" />
+    </div>
 
-    <input
-      v-model="email"
-      type="text"
-      placeholder="Email address"
-      class="text-inputs h-12 w-full border border-zinc-700 bg-transparent px-4 py-3"
-      @keyup.enter="emit('apply', { email })"
-    />
-  </div>
-
-  <Button :text="button" primary rounded class="mt-32 w-full" @click="emit('apply', { email })" />
+    <Button :text="button" primary rounded class="mt-32 w-full" type="submit" />
+  </VeeForm>
 </template>
 
 <style scoped></style>
