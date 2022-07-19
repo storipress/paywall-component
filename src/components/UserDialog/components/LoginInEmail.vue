3dc3ef307fae55b38f7789318faac8eeb311ec21
DanSnow <dododavid006@gmail.com>
2022-07-19 14:14:06 +0800

    style: format

--- src/components/UserDialog/components/LoginInEmail.vue
+++ src/components/UserDialog/components/LoginInEmail.vue
@@ -27,23 +27,23 @@ const onSubmit = function (formData: any, submitEvent: any) {
 <template>
   <VeeForm :validation-schema="schema" @submit="onSubmit">
     <div class="relative flex-auto" v-bind="$attrs">
-      <Button primary rounded color-hex="#ffffff" class="layer-1 w-full mb-6" type="button">
+      <Button primary rounded color-hex="#ffffff" class="layer-1 mb-6 w-full" type="button">
         <template #buttonText>
-          <Email class="text-zinc-700 w-6 h-6" />
-          <span class="text-button text-zinc-700 w-full pr-6">Log in with Email</span>
+          <Email class="h-6 w-6 text-zinc-700" />
+          <span class="text-button w-full pr-6 text-zinc-700">Log in with Email</span>
         </template>
       </Button>
       <VeeField
         name="email"
         type="email"
         placeholder="Email address"
-        class="text-inputs border-zinc-700 w-full h-12 px-4 py-3 bg-transparent border"
+        class="text-inputs h-12 w-full border border-zinc-700 bg-transparent px-4 py-3"
         @keyup.enter="onSubmit"
       />
       <VeeErrorMessage as="div" name="email" class="text-caption absolute bottom-[calc(-1.5_*_1em)] text-red-700" />
     </div>
 
-    <Button :text="button" primary rounded class="w-full mt-32" type="submit" />
+    <Button :text="button" primary rounded class="mt-32 w-full" type="submit" />
   </VeeForm>
 </template>
 
